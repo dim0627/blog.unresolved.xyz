6ce92f4f2314a4c9dfcd6e5fb56f71fa10872598
Daisuke Tsuji <dim0627@gmail.com>
2018-07-11 21:35:39 +0900

    fixed #11.

--- src/templates/post.js
+++ src/templates/post.js
@@ -5,9 +5,13 @@ import styled from 'styled-components'
 import Hero from '../components/Hero'
 import Container from '../components/Container'
 import Author from '../components/Author'
+import Share from '../components/Share'
 import variables from '../styles/variables'
+import settings from '../settings/settings'
 
 const ArticleBody = styled.div`
+  margin-bottom: 4rem;
+
   h2 {
     position: relative;
     margin: 4rem 0 2rem;
@@ -16,7 +20,7 @@ const ArticleBody = styled.div`
     font-size: 1.6rem;
     font-weight: 700;
     line-height: 2.5rem;
-    background: -webkit-linear-gradient(0deg, ${variables.colorMain}, ${variables.colorAccent});
+    background: linear-gradient(0deg, ${variables.colorMain}, ${variables.colorAccent});
     -webkit-background-clip: text;
     -webkit-text-fill-color: transparent;
 
@@ -27,7 +31,7 @@ const ArticleBody = styled.div`
       left: 0;
       width: 8rem;
       height: 4px;
-      background: -webkit-linear-gradient(0deg, ${variables.colorMain}, ${variables.colorAccent});
+      background: linear-gradient(0deg, ${variables.colorMain}, ${variables.colorAccent});
       transform: perspective(70px) rotateY(15deg) rotate(-3deg);
     }
   }
@@ -91,6 +95,7 @@ class PostTemplate extends React.Component {
         <Hero title={post.title} sub={post.date} background={post.heroPhoto.file.url} heading={true} />
         <Container>
           <ArticleBody dangerouslySetInnerHTML={{__html: post.body.childMarkdownRemark.html}} />
+          <Share title={post.title} url={`${settings.site.siteUrl}/${post.slug}`} />
           <Author author={post.author} />
         </Container>
       </div>
