56cbcc8cbcb3a297c250b7879c968bb60f27ec6b
Daisuke Tsuji <dim0627@gmail.com>
2019-05-25 09:49:30 +0900

    :ok_hand: remove all assets.

--- src/components/Container.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components'
-
-export default styled.div`
-  max-width: 46rem;
-  margin: 0 auto;
-  padding: 0 1rem;
-`
--- src/components/Disqus.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Disqus from 'disqus-react';
-import settings from '../settings/settings'
-
-export default (config) => (
-  <Disqus.DiscussionEmbed
-    shortname={settings.disqus.shortName}
-    config={config} />
-)
