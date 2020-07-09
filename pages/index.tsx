436c9595f9a0e79e24e095a8b60c9ffd47537ed0
Daisuke Tsuji <dim0627@gmail.com>
2020-07-10 08:56:35 +0900

    :ok_hand: thru contentful api.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,11 @@
 import Head from 'next/head'
 
-// const client = require('contentful').createClient({
-//   space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
-//   accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN
-// })
+const client = require('contentful').createClient({
+  space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
+  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN
+})
 
-async function fetchEntries() {
-  console.log(process.env)
-  // const entries = await client.getEntries()
-  // if (entries.items) return entries.items
-  // console.log(`Error getting Entries for ${contentType.name}.`)
-}
-
-export default function Home() {
-  fetchEntries().then(items => {
-    console.log(items)
-  })
-
+export default function Home({ posts }) {
   return (
     <div className="container">
       <Head>
@@ -28,6 +17,7 @@ export default function Home() {
         <h1 className="title">
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+        {posts.map(post => <div>{post.title}</div>)}
       </main>
 
       <style jsx global>{`
@@ -46,4 +36,16 @@ export default function Home() {
       `}</style>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const contentType = await client.getContentTypes()
+  const postContentId = contentType.items.find(contentTypeItem => contentTypeItem.name === 'Post').sys.id
+
+  const entries = await client.getEntries({ content_type: postContentId })
+  const posts = entries.items.map(item => ({ title: item.fields.title, slug: item.fields.slug }))
+
+  return {
+    props: { posts }
+  }
 }
