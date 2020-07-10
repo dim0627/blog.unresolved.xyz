e84a0455088dcdfc804b69b7878a1ed82b957829
Daisuke Tsuji <dim0627@gmail.com>
2020-07-10 19:27:07 +0900

    :ok_hand: thru.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,14 +8,17 @@ const Footer = () => (
       <ul className={styles.featuredList}>
         <li>
           Hosted on
+          {' '}
           <a href="https://vercel.com/" className={styles.link}>Vercel</a>
         </li>
         <li>
           Build by
+          {' '}
           <a href="https://nextjs.org/" className={styles.link}>Next.js</a>
         </li>
         <li>
           Designed by
+          {' '}
           <a href="https://yet.unresolved.xyz" className={styles.link}>Daisuke Tsuji</a>
         </li>
       </ul>
