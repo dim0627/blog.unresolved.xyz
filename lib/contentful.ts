e89e390c5f86888ca2aefaee7b6f54477f05c148
Daisuke Tsuji <dim0627@gmail.com>
2021-10-16 10:12:56 +0900

    indexing.

--- lib/contentful.ts
+++ lib/contentful.ts
@@ -3,12 +3,30 @@ const client = require('contentful').createClient({
   accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
 });
 
+interface Post {
+  title: string
+  slug: string
+  body: string
+  heroPhoto: {
+    fields: {
+      file: {
+        url: string
+      }
+    }
+  }
+  author: {
+    fields: {
+      name: string
+    }
+  }
+}
+
 const getPostContentId = async () => {
   const contentType = await client.getContentTypes();
   return contentType.items.find((contentTypeItem) => contentTypeItem.name === 'Post').sys.id;
 };
 
-export const getPosts = async (options = {}) => {
+export const getPosts = async (options = {}): Promise<{ fields: Post }[]> => {
   const postContentId = await getPostContentId();
   const entries = await client.getEntries({ content_type: postContentId, ...options });
   return entries.items;
